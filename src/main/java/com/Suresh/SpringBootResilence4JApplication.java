399fa7ca7a52bb8d4778702114f493da777205d9
SureshKumarReddy <sureshkumarreddyraina@gmail.com>
2021-12-09 07:26:09 +0530

    first commit

---
+++ src/main/java/com/Suresh/SpringBootResilence4JApplication.java
@@ -0,0 +1,13 @@
+package com.Suresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResilence4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResilence4JApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Suresh/SpringBootResilence4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Suresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResilence4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
